213ede59221f32859a5047875042866b7685a7f7
eddieugly <eddie.ugly04@gmail.com>
2023-01-24 04:01:21 -0800

    Added NProgress

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,18 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
+  progress: {
+
+    // The color of the progress bar.
+    color: 'red',
+
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
+  },
   resolve: name => require(`./Pages/${name}.vue`),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
-})
+});
